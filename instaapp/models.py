04af4b23fa255276f0d92f026c2b2bef4a9aed68
Steve-design <smzalendo31@gmail.com>
2019-11-15 16:03:17 +0300

    create a model for the profile

--- instaapp/models.py
+++ instaapp/models.py
@@ -4,3 +4,11 @@ from tinymce.models import HTMLField
 import datetime as dt
 
 # Create your models here.
+
+class Profile(models.Model):
+    bio = HTMLField()
+    avatar = models.ImageField(upload_to='images/', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+
+    def save_profile(self):
+        self.save()
