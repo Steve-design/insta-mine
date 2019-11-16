f26aec146983ea1f6870370b40f0b42bfd999183
Steve-design <smzalendo31@gmail.com>
2019-11-16 11:53:29 +0300

    add a class for NewsLetterForm

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,2 +1,6 @@
 from django import forms
 from .models import *
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name', max_length=30)
+    email = forms.EmailField(label='Email')
