620ef557420935a477615a16b8ba036d193f5e19
Steve-design <smzalendo31@gmail.com>
2019-11-16 11:02:01 +0300

    import all required modules

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+import datetime as dt
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
+from .email import *
 
 # Create your views here.
