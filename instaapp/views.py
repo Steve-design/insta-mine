a8c7a75941e89c65c4989bb713fc02c2f827c794
Steve-design <smzalendo31@gmail.com>
2019-11-16 11:25:19 +0300

    add an add comment function

--- instaapp/views.py
+++ instaapp/views.py
@@ -98,4 +98,20 @@ def like(request,operation,pk):
     elif operation =='unlike':
         image.likes -= 1
         image.save()
-    return redirect('homepage')             
+    return redirect('homepage')   
+
+@login_required(login_url='/accounts/login/')
+def add_comment(request,pk):
+    image = get_object_or_404(Post, pk=pk)
+    current_user = request.user
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.imagecommented = image
+            comment.poster = current_user
+            comment.save()
+            return redirect('homepage')
+    else:
+        form = CommentForm()
+        return render(request,'comment.html',{"user":current_user,"comment_form":form})              
