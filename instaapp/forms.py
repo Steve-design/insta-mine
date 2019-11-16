e9f42c1209b253d5dc0f428ab42811b239aa70ec
Steve-design <smzalendo31@gmail.com>
2019-11-16 11:54:31 +0300

    add a comment form class

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -4,3 +4,9 @@ from .models import *
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name', max_length=30)
     email = forms.EmailField(label='Email')
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        exclude = ['poster', 'imagecommented']    
